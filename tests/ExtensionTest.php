3e96b16f4059a3a4a6b75a6cdf4fd9360acd921e
Leandro G. Gehlen <leandrogehlen@gmail.com>
2019-11-12 16:45:05 -0300

    Added tests

---
+++ tests/ExtensionTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yiiunit\smarty;
+
+use Yii;
+use yii\helpers\FileHelper;
+use yii\web\View;
+
+/**
+ * @group smarty
+ */
+class ExtensionTest extends TestCase
+{
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->mockWebApplication();
+        FileHelper::createDirectory(Yii::getAlias('@runtime/Smarty'));
+    }
+
+    protected function tearDown()
+    {
+        parent::tearDown();
+        FileHelper::removeDirectory(Yii::getAlias('@runtime/Smarty'));
+    }
+
+    public function testJsExpr()
+    {
+        $renderer = new TestViewRenderer();
+        $smarty = $renderer->getSmartyInstance();
+        $this->assertArrayHasKey('jsexpr', $smarty->registered_plugins['function']);
+    }
+}
+
+class TestViewRenderer extends \yii\smarty\ViewRenderer {
+
+    /**
+     * @return \Smarty
+     */
+    public function getSmartyInstance()
+    {
+        return $this->smarty;
+    }
+}
